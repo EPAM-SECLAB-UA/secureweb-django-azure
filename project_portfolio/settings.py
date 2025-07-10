a8897d28fa5ac37bed37ca91c267bb5b3f5f01d2
Vitalii Shevchuk <126573180+VitaliiShevchuk2023@users.noreply.github.com>
2025-07-10 06:57:39 +0300

    Update settings.py

--- project_portfolio/settings.py
+++ project_portfolio/settings.py
@@ -27,7 +27,14 @@ SECRET_KEY = config("SECRET_KEY", default='')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+#ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+
+ALLOWED_HOSTS = [
+    'django-app-budget-1752082786.azurewebsites.net',
+    'localhost',
+    '127.0.0.1',
+    '.azurewebsites.net',  # Для всіх Azure subdomain
+]
 
 if 'CODESPACE_NAME' in os.environ:
     codespace_name = config("CODESPACE_NAME")
